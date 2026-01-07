cb6779dca28c0f101046bff828ffe12bf62d23f1
ErlanEffendy <erlaneffendy78@gmail.com>
2026-01-07 10:25:07 +0700

    add cors

--- api.py
+++ api.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from ocr import KTPExtractor, NPWPExtractor
 import os
 import uuid
 import shutil
 
 app = FastAPI(title="KTP & NPWP OCR API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 ktp_extractor = KTPExtractor()
 npwp_extractor = NPWPExtractor()
 
